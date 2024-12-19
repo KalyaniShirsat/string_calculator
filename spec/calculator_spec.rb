a947ff0ecb50628a6943af8afe6abb07e92ae0ee
Kalyani Shirsat <kalyanishirsat@gmail.com>
2024-12-20 01:49:34 +0530

    RSpec - String calculator

---
+++ spec/calculator_spec.rb
@@ -0,0 +1,29 @@
+require 'calculator'
+
+RSpec.describe Calculator do
+  context 'when input is a string of non-negative numbers' do
+    context 'when the string is a single number' do
+      it 'returns the number' do
+        expect(Calculator.add('7')).to eq 7
+      end
+    end
+
+    context 'when the delimiter is a comma' do
+      it 'returns sum of the numbers in the string' do
+        expect(Calculator.add('5,1,0,2,3')).to eq 11
+      end
+    end
+
+    context 'when the delimiter is a newline character' do
+      it 'returns sum of the numbers in the string' do
+        expect(Calculator.add('5\n1,0,2,2')).to eq 10
+      end
+    end
+  end
+
+  context 'when input is an empty string' do
+    it 'returns 0' do
+      expect(Calculator.add('')).to eq 0
+    end
+  end
+end
