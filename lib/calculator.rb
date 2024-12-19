901c3414bcae45658e3ae03e40c22d2c30627f1c
Kalyani Shirsat <kalyanishirsat@gmail.com>
2024-12-20 02:01:39 +0530

    Handle negative numbers input

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,7 +2,16 @@ class Calculator
   def self.add(numbers_string)
     return 0 if numbers_string.empty?
 
-    # Add the numbers
-    numbers_string.gsub('\n', ',').split(',').sum(&:to_i)
+    # Split string into numbers
+    numbers = numbers_string.gsub('\n', ',').split(',')
+
+    # Check for negative numbers
+    negative_numbers = numbers.select { |number| number.to_i.negative? }
+    if negative_numbers.any?
+      raise ArgumentError, "negative numbers not allowed #{negative_numbers.join(', ')}"
+    end
+
+    # Add the non-negative numbers
+    numbers.sum(&:to_i)
   end
 end
